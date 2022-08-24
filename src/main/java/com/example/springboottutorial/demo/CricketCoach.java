e36500979d3106aafcce6c7892a3eae91fbea55d
Shantanu Hirlekar <2hshantanu@gmail.com>
2022-08-23 23:47:58 -0400

    Demo

--- src/main/java/com/example/springboottutorial/demo/CricketCoach.java
+++ src/main/java/com/example/springboottutorial/demo/CricketCoach.java
@@ -1,7 +1,5 @@
 package com.example.springboottutorial.demo;
 
-import com.example.springboottutorial.demo.Coach;
-
 public class CricketCoach implements Coach {
     public String getDailyWorkOut(){
         return "Spend 30 minutes bowling.";
---
+++ src/main/java/com/example/springboottutorial/demo/SwimmingCoach.java
@@ -0,0 +1,8 @@
+package com.example.springboottutorial.demo;
+
+public class SwimmingCoach implements Coach {
+
+    public String getDailyWorkOut() {
+        return "Swim 2000 meters of freestyle";
+    }
+}
